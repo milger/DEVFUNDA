d591cea8bcdeee124a9f3aeb3d0b7932c552f03c
Jimena Sanabria Tames <jsanabria@pros.com>
2015-09-25 10:25:39 -0400

    Update import csv utility according last observations

--- movie_rental_store/src/utils/csv_handler/import_csv_file.py
+++ movie_rental_store/src/utils/csv_handler/import_csv_file.py
@@ -2,33 +2,63 @@
 
 __author__ = 'JimenaSanabria'
 
+import os
 import csv
 
-def parse_csv_file(file_name):
+
+def get_file_name(file_path):
+    """Get the file name of a path.
+
+    Keyword arguments:
+    file_path -- the str with the path of file
+
+    Return a string with the name of file
+
+    """
+
+    head, tail = os.path.split(file_path)
+    return tail
+
+
+def get_file_data(reader_file):
+    """Get a list of string dictionary for the data of a file
+
+    Keyword arguments:
+    reader_file -- the reader object of csv
+
+    Return a list with dictionary of string with the data of header:row value
+
+    """
+
+    complete_data_list = []
+    for row in reader_file:
+        complete_data_list.append(row)
+
+    return complete_data_list
+
+
+def parse_csv_file(file_path):
     """Parse the csv file.
 
     Keyword arguments:
-    file_name -- the str with the name of csv file to be parsed
+    file_path -- the str with the path of file
 
     Return an array with dictionary of string after parsing a csv file
 
     """
-    complete_data_list = []
-    file = file_name + ".csv"
-    import_file = open(file, "rb")
-    reader_file = csv.reader(import_file)
-    row_index = 0
-    for row in reader_file:
-        row_data = {}
-        if row_index == 0:
-            header = row
-        else:
-            column_index = 0
-            for column in row:
-                row_data[header[column_index]] = column
-                column_index += 1
-            complete_data_list.append(row_data)
-        row_index += 1
 
-    import_file.close()
+    complete_data_list = []
+    file_name = get_file_name(file_path)
+
+    try:
+        import_file = open(file_name, "rb")
+
+    except IOError:
+        print 'An error occured trying to read the file.'
+
+    else:
+        reader_file = csv.DictReader(import_file)
+        complete_data_list = get_file_data(reader_file)
+        import_file.close()
+
     return complete_data_list
--- movie_rental_store/tests/csv_tests/import_csv_test.py
+++ movie_rental_store/tests/csv_tests/import_csv_test.py
@@ -4,21 +4,46 @@ import sys
 sys.path.append('../../src')
 
 import unittest
+import csv
+from utils.csv_handler.import_csv_file import get_file_name
+from utils.csv_handler.import_csv_file import get_file_data
 from utils.csv_handler.import_csv_file import parse_csv_file
+
 
 class ImportCsvTest(unittest.TestCase):
     """Unit Tests to test method from import_csv_file utility."""
 
-    def test_import_csv_file_empty(self):
-        """Test the parse of an emty csv file"""
+    def test_get_file_name_distinct_folder(self):
+        """Test get file name when that is other folder"""
 
-        complete_data = parse_csv_file("test_empty")
+        file_name = get_file_name("csv_files/test.csv")
+        self.assertEqual("test.csv", file_name)
+
+    def test_get_file_name_from_same_folder(self):
+        """Test get file name when that is in same folder"""
+
+        file_name = get_file_name("test_empty.csv")
+        self.assertEqual("test_empty.csv", file_name)
+
+    def test_get_data_of_file(self):
+        """Test get a list with dictionary (key:value)"""
+
+        import_file = open("test.csv", "rb")
+        reader_file = csv.DictReader("test.csv")
+        complete_data = get_file_data(reader_file)
+        import_file.close()
+        self.assertNotEqual([], complete_data)
+
+    def test_import_csv_file_empty(self):
+        """Test the parse of an empty csv file"""
+
+        complete_data = parse_csv_file("test_empty.csv")
         self.assertEqual([], complete_data)
 
     def test_import_csv_file_filled(self):
         """Test the parse of csv file"""
 
-        complete_data = parse_csv_file("test")
+        complete_data = parse_csv_file("test.csv")
         self.assertNotEqual([], complete_data)
 
     def test_import_csv_file_first_row(self):
@@ -27,7 +52,7 @@ class ImportCsvTest(unittest.TestCase):
         expected_data = {"Measure":"SIM_BASE_PRICE1",
                          "Scope":"VP02/Arlington",
                          "Error":"The formula could not be evaluated"}
-        complete_data = parse_csv_file("test")
+        complete_data = parse_csv_file("csv_files/test.csv")
         self.assertEqual(expected_data, complete_data[0])
 
     def test_import_csv_file_last_row(self):
@@ -36,9 +61,8 @@ class ImportCsvTest(unittest.TestCase):
         expected_data = {"Measure":"SIM_BASE_PRICE4",
                          "Scope":"VP01/Arlington",
                          "Error":"The formula could not be evaluated"}
-        complete_data = parse_csv_file("test")
+        complete_data = parse_csv_file("test.csv")
         self.assertEqual(expected_data, complete_data[3])
 
 if __name__ == "__main__":
     unittest.main()
-
